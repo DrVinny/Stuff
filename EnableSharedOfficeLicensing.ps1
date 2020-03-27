a039af9d0671363da054526e79d45786212dc441
DrVinnyBoombatz <50646492+DrVinny@users.noreply.github.com>
2020-03-26 20:12:34 -0700

    Add files via upload

---
+++ EnableSharedOfficeLicensing.ps1
@@ -0,0 +1,7 @@
+# Enable Shared Office Licensing on a local machine
+# Created 2020.03.10
+# MV
+
+
+# This checks for the existence of the reg value "SharedComputerLicensing" under HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Office\ClickToRun\Configuration.
+
