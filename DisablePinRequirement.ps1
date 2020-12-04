a72a2ef97974969045dc0d31489de17e973737f0
DrVinnyBoombatz <50646492+DrVinny@users.noreply.github.com>
2020-12-04 14:14:26 -0700

    Create DisablePinRequirement.ps1

---
+++ DisablePinRequirement.ps1
@@ -0,0 +1,9 @@
+$path = "HKLM:\SOFTWARE\Policies\Microsoft"
+$key = "PassportForWork"
+$name = "Enabled"
+$value = "0"
+ 
+New-Item -Path $path -Name $key –Force
+ 
+New-ItemProperty -Path $path\$key -Name $name -Value $value -PropertyType DWORD -Force
+ 
